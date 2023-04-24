772f18b511e1399f0d9cc945b9fdf8af8ad53359
Makki1202 <101123396+Makki1202@users.noreply.github.com>
2023-04-24 12:18:08 +0200

    Add files via upload

---
+++ IO.h
@@ -0,0 +1,12 @@
+//
+// Created by skakm on 24/04/2023.
+//
+
+#ifndef PROJEKT2V2_MACHINE_TA_IO_H
+#define PROJEKT2V2_MACHINE_TA_IO_H
+
+#endif //PROJEKT2V2_MACHINE_TA_IO_H
+#define MAX_LIMIT 50
+char IOmessage[MAX_LIMIT];
+int writefile(char* filename);
+int readfile(char* filename);
